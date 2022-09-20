ad9d56638e482ec3d1fcdf3e2c6478f360a98e28
diana <volchenko_93@mail.ru>
2022-09-20 15:45:01 +0300

    Add all folders

---
+++ task10/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное 
+число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int getSecondNumber(int number)
+{
+    int firstNumber = number / 10;
+    int result = firstNumber % 10;
+    return result;
+}
+
+int randomNumber = new Random().Next(100, 1000);
+int secondNumber = getSecondNumber(randomNumber);
+Console.WriteLine($"Вторая цифра числа {randomNumber} равна {secondNumber}");
---
+++ task11/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Напишите программу, которая выводит третью цифру 
+заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+int getThirdNumber(int someNumber)
+{   
+    int thirdNumber = 0;
+    while ( someNumber > 999 )
+    {
+        someNumber = someNumber / 10;
+    }
+    if ( someNumber >= 100 &&  someNumber <= 1000 )
+    {
+        thirdNumber = someNumber % 10;
+    }
+    else
+    {   
+        Console.WriteLine("Третьей цифры нет");
+        thirdNumber = -1;
+    }
+    return thirdNumber;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = getThirdNumber(number);
+Console.WriteLine(result);
