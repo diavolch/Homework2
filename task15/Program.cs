fa67a6d8ceec121736df597f060e78b54cc8bc5a
diana <volchenko_93@mail.ru>
2022-09-20 15:41:29 +0300

    Готовое домашнее задание

---
+++ task15/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+string getDayOff(int dayNumber)
+{
+    string answer = "";
+    if ( dayNumber == 6 || dayNumber == 7 )
+    {
+        answer = "Да";
+    }
+    else 
+    {
+        answer = "Нет";
+    }
+    return answer;
+}
+
+Console.Write("Введите цифру от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+string aboutDay = getDayOff(day);
+Console.WriteLine(aboutDay);
